ec9777b0b54caeec4a37facc3f25e6fc19ebdd01
Iwer Petersen <iwer.petersen@haw-hamburg.de>
2014-09-26 16:39:00 +0200

    Removed source files from Application project that have moved to Static Library project

--- src/typedefs.h
+++
@@ -1,7 +0,0 @@
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-
-typedef pcl::PointXYZRGBA PointType;
-typedef pcl::PointCloud<PointType> Cloud;
-typedef Cloud::ConstPtr CloudConstPtr;
-typedef Cloud::Ptr CloudPtr;
